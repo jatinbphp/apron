7f344e4105f3d2cb5e8d5b4c65a43a7717a2dbc5
Jatin Bhatt <jatin.b.php@gmail.com>
2022-02-23 20:34:09 +0530

    title removed from message popup

--- src/app/providers/send-receive-requests.service.ts
+++ src/app/providers/send-receive-requests.service.ts
@@ -218,7 +218,7 @@ export class SendReceiveRequestsService
 	{	
 		const alert = await this.alertCtrl.create(
 		{
-		header: 'THE APRON',
+		//header: 'THE APRON',
 		message: message,
 		buttons: 
 		[
